1c30fc2ac3469452b5d5751da674c55b633e8960
Denis A. Potapov <denisdebron@gmail.com>
2026-07-01 10:12:06 +0300

    feat(dashboard-page): connect app-panel-list component

--- src/app/features/dashboard-page/dashboard-page.component.ts
+++ src/app/features/dashboard-page/dashboard-page.component.ts
@@ -10,10 +10,17 @@ import { PanelListComponent } from '@features/smart-components/panels/panel-list
  */
 @Component({
     selector: 'app-dashboard-page',
-    imports: [MetricsOverviewComponent, PanelListComponent],
+    imports: [
+        MetricsOverviewComponent,
+        PanelListComponent
+    ],
     templateUrl: './dashboard-page.component.html',
     styleUrl: './dashboard-page.component.scss'
 })
 export class DashboardPageComponent {
     private readonly router = inject(Router);
+
+    public onPanelSelected(panelId: string): void  {
+        this.router.navigate(['/panels', panelId]);
+    }
 }
